c504e0d6002abf3f96662394f2ae92f03f976bab
Nico Hämäläinen <nico@sizeof.io>
2016-03-18 15:29:23 +0200

    Updates and final

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+// Connect module
+const cookieSession = require('cookie-session');
+
+const cookieSettings = {
+  secret: 'testing',
+  domain: '.session.dev'
+};
+
 // Set up express server
 const express = require('express');
 const expressApp = express();
 
+// Configure express session
+expressApp.use(cookieSession(cookieSettings));
+
 // Express index route
 expressApp.get('/', function (req, res) {
-  res.send('express.');
+  // Get session
+  const session = req.session;
+  // Increase counter
+  var n = session.views || 0;
+  session.views = ++n;
+
+  res.send('express.' + n);
 });
 
 // Start express server
@@ -14,15 +31,27 @@ expressApp.listen(7000, function() {
 
 // Set up koa server
 const koa = require('koa');
+const koaConnect = require('koa-connect');
 const koaApp = koa();
+
+koaApp.use(koaConnect(cookieSession(cookieSettings)));
 
 // Koa index route
 koaApp.use(function *(next) {
-  this.body = 'koa.';
+  if (this.path === '/favicon.ico') return;
+
+  // Get session
+  const session = this.req.session;
+
+  // Increase counter
+  var n = session.views || 0;
+  session.views = ++n;
+
+  this.body = 'koa.' + n;
   yield next;
 });
 
 // Start koa server
 koaApp.listen(7001, function() {
-  console.log('Koa server started on *:7001');  
+  console.log('Koa server started on *:7001');
 });
